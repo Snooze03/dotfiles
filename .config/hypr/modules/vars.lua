9222622440ac6541b81b4b3a02378386a9703de9
Snooze03 <baayjohnzeus@gmail.com>
2026-07-12 08:33:46 +0800

    Added: monitor variables

--- .config/hypr/modules/vars.lua
+++ .config/hypr/modules/vars.lua
@@ -1,7 +1,12 @@
 return {
 	programs = {
 		terminal = "foot",
-		fileManager = "thunar",
+		-- fileManager = "thunar",
+		fileManager = "yazi",
 		menu = "rofi -show drun",
+	},
+	monitors = {
+		external = "HDMI-A-1",
+		internal = "eDP-1",
 	},
 }
